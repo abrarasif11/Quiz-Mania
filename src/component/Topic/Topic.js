0fac459390d12abb829be824a6287a3c296c72a9
abrarasif11 <faasif929@gmail.com>
2022-10-11 01:40:03 +0600

    added-topics-name

--- src/component/Topic/Topic.js
+++ src/component/Topic/Topic.js
@@ -8,7 +8,7 @@ const Topic = ({quiz}) => {
 	<img src={quiz.logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
 	<div className="flex flex-col justify-between p-6 space-y-8">
 		<div className="space-y-2">
-			<h2 className="text-3xl font-semibold tracking-wide">Name : {quiz.name}</h2>
+			<h2 className="text-3xl font-semibold tracking-wide">{quiz.name}</h2>
 			
 		</div>
 		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-violet-400 text-gray-900">Read more</button>
