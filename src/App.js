6411b8a07b45ba072ed023ee4179a080e988e6be
abrarasif11 <faasif929@gmail.com>
2022-10-10 23:54:05 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './component/Blog/Blog';
+import Error from './component/Error/Error';
+import Home from './component/Home/Home';
+import Main from './component/layout/Main';
+import Statics from './component/Statitcs/Statics';
+import Topics from './component/Topics/Topics';
 
 function App() {
-  const router = () =>{
-
-  }
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element : <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element: <Home></Home>
+        },
+      {
+        path: '/topics',
+        element : <Topics></Topics>
+      },
+      {
+        path: '/statics',
+        element : <Statics></Statics>
+      },
+      {
+        path : '/blog',
+        element : <Blog></Blog>
+      }
+      ],
+    },
+    {
+      path: '*',
+      element: <Error></Error>
+    }
+  ])
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>  
---
+++ src/component/Header/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <header className="p-4 bg-slate-200 dark:text-gray-100">
+	<div className="container flex justify-between h-16 mx-auto">
+		<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+			<h2 className='text-4xl font-bold'>Quiz Times</h2>
+		</a>
+		<ul className="items-stretch hidden space-x-3 md:flex">
+			<li className="flex">
+				<Link to={'/'}>Home</Link>
+			</li>
+			<li className="flex">
+				<Link to={'/topics'}>Topics</Link>
+			</li>
+			<li className="flex">
+				<Link to={'/statics'}>Statics</Link>
+			</li>
+			<li className="flex">
+				<Link to={'/blog'}>Blog</Link>
+			</li>
+		</ul>
+		<button className="flex justify-end p-4 md:hidden">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
+				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+			</svg>
+		</button>
+	</div>
+</header>
+        </div>
+    );
+};
+
+export default Header;
