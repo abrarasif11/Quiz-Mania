0c9c0ae570a6791d682a3dd8b2b0470e2ed78009
abrarasif11 <faasif929@gmail.com>
2022-10-10 22:34:05 +0600

    router-setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = () =>{
+
+  }
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>  
     </div>
   );
 }
